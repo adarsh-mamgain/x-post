c2a03676d9ae06f8316292f85cf88737afe7699c
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2024-05-07 02:01:23 +0530

    feat: Add Dashboard page with ScheduleCard component

---
+++ app/api/auth/[slug]/twitter/route.ts
@@ -0,0 +1,53 @@
+import { client } from "@/lib/twitter";
+import { createSession, getSession } from "@/utils/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  if (
+    req.method === "GET" &&
+    req.nextUrl.pathname === "/api/auth/signin/twitter"
+  ) {
+    const result = client.generateOAuth2AuthLink(
+      "http://localhost:3000/api/auth/callback/twitter",
+      {
+        scope: [
+          "tweet.read",
+          "tweet.write",
+          "tweet.moderate.write",
+          "users.read",
+          "offline.access",
+        ],
+      }
+    );
+
+    await createSession("codeVerifier", result.codeVerifier);
+    return NextResponse.json(result);
+  } else if (
+    req.method === "GET" &&
+    req.nextUrl.pathname === "/api/auth/callback/twitter"
+  ) {
+    const { searchParams } = new URL(req.url);
+    const code = searchParams.get("code");
+    const state = searchParams.get("state");
+
+    if (!code || !state) {
+      return NextResponse.json(
+        { error: "Missing required parameters" },
+        { status: 400 }
+      );
+    }
+
+    const codeVerifier = await getSession("codeVerifier");
+    const loggedClient = await client.loginWithOAuth2({
+      redirectUri: "http://localhost:3000/api/auth/callback/twitter",
+      codeVerifier,
+      code,
+    });
+
+    // console.log("Logged client:", code, state, loggedClient);
+
+    return NextResponse.redirect(process.env.URL || "");
+  } else {
+    return NextResponse.json({ message: "No action performed" });
+  }
+};
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import ScheduleCard from "@/components/ScheduleCard";
+
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ScheduleCard />
+    </main>
+  );
+}
