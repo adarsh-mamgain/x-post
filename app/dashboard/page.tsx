b4611ca0955cbce42edb205299db917c99bd1210
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2024-05-18 01:39:12 +0530

    Refactor Dashboard page component and update session handling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,25 @@
 import ScheduleCard from "@/components/ScheduleCard";
 import { getSession, validateUserSession } from "@/utils/server";
-import { redirect } from "next/navigation";
 
-export default async function Dashboard() {
+export async function getServerSideProps() {
   const session = await getSession("jwt");
   const validatedSession = await validateUserSession(session);
 
-  if (!(session && validatedSession)) return redirect("/");
+  if (!(session && validatedSession)) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
+
+export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ScheduleCard />
